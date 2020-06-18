dcb642a9ef1d1c3f0576d4914fdb98cdda11babb
tnsyn <e0425292@u.nus.edu>
2020-06-18 21:00:14 +0800

    Update seeds.js

--- seeds.js
+++ seeds.js
@@ -1,170 +1,32 @@
 const mongoose = require("mongoose");
 const Event = require("./models/event");
+const Group = require("./models/group");
+const User = require("./models/user");
 
-const data = [
+const events = [
     {
         name: "The Square Small Business Hackathon",
         url: "https://square.devpost.com/?ref_feature=challenge&ref_medium=home",
         minGroupSize: 1,
-        maxGroupSize: 5,
-        groups: [
-          {
-            name: "Group 1",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          },
-          {
-            name: "Group 2",
-            size: 1,
-            users: [
-              {
-                name: "Rachel",
-              }
-            ]
-          },
-          {
-            name: "Group 3",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          }
-        ]
+        maxGroupSize: 5
     },
     {
         name: "HackBangPH",
         url: "https://hackbangph.devpost.com/?ref_feature=challenge&ref_medium=home",
         minGroupSize: 2,
-        maxGroupSize: 4,
-        groups: [
-          {
-            name: "Group 1",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          },
-          {
-            name: "Group 2",
-            size: 1,
-            users: [
-              {
-                name: "Rachel",
-              }
-            ]
-          },
-          {
-            name: "Group 3",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          }
-        ]
+        maxGroupSize: 4
     },
     {
-        name: "Facebook Hackathon: AI",
-        url: "https://fbai2.devpost.com/?ref_feature=challenge&ref_medium=home",
-        minGroupSize: 1,
-        maxGroupSize: 4,
-        groups: [
-          {
-            name: "Group 1",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          },
-          {
-            name: "Group 2",
-            size: 1,
-            users: [
-              {
-                name: "Rachel",
-              }
-            ]
-          },
-          {
-            name: "Group 3",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          }
-        ]
+       name: "Facebook Hackathon: AI",
+       url: "https://fbai2.devpost.com/?ref_feature=challenge&ref_medium=home",
+       minGroupSize: 1,
+       maxGroupSize: 4
     },
     {
-        name: "Airtable Block Contest",
-        url: "https://airtable.devpost.com/?ref_feature=challenge&ref_medium=home",
-        minGroupSize: 1,
-        maxGroupSize: 1,
-        groups: [
-          {
-            name: "Group 1",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          },
-          {
-            name: "Group 2",
-            size: 1,
-            users: [
-              {
-                name: "Rachel",
-              }
-            ]
-          },
-          {
-            name: "Group 3",
-            size: 2,
-            users: [
-              {
-                name: "Rachel",
-              },
-              {
-                name: "John"
-              }
-            ]
-          }
-        ]
+      name: "Airtable Block Contest",
+      url: "https://airtable.devpost.com/?ref_feature=challenge&ref_medium=home",
+      minGroupSize: 1,
+      maxGroupSize: 1
     }
 ];
 
@@ -174,17 +36,46 @@ function seedDB(){
             console.log(err);
         } else {
             console.log("REMOVED EVENTS")
-            data.forEach((seed) => {
+            events.forEach((seed) => {
                 Event.create(seed, (err, event) => {
                     if(err){
                         console.log(err);
-                    } else{
-                        console.log("added event");
+                    } else {
+                      Group.create(
+                        {
+                          name: "Group 1",
+                          size: 1
+                        }, (err, group) => {
+                          if (err) {
+                            console.log(err)
+                          } else {
+                            User.create(
+                              {
+                                name: "Rachel"
+                              }, (err, user) => {
+                                if(err) {
+                                  console.log(err)
+                                } else {
+                                  group.users.push(user)
+                                  group.save()
+                                  console.log("Created group")
+                                  event.groups.push(group)
+                                  event.save()
+                                  console.log("Created event")
+                                }
+                              }
+                            )
+                          }
+                        }
+                      )
                     }
-                })
-            })
+                  }
+                )
+              }
+            )
+          }
         }
-    });
-}
+      )
+    }
 
 module.exports = seedDB;
