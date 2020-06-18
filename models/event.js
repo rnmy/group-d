64c6901f8e541bc25584932e4f56b0833bcbcae9
tnsyn <e0425292@u.nus.edu>
2020-06-18 20:22:58 +0800

    Update event.js

--- models/event.js
+++ models/event.js
@@ -4,7 +4,13 @@ const eventSchema = new mongoose.Schema({
     name: String,
     url: String,
     minGroupSize: Number,
-    maxGroupSize: Number
+    maxGroupSize: Number,
+    groups: [
+      {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: "Group"
+      }
+    ]
 })
 
 module.exports = mongoose.model("Event", eventSchema);
