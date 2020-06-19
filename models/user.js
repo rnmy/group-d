f83b0ae662f10a162600e625ab1a86b210b0e3fe
rnmy <e0439305@u.nus.edu>
2020-06-19 10:12:36 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String
+  name: String,
+  email: String,
+  username: String,
+  password: String,
+  organization: String,
+  profilePic: String
 })
+
+userSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model("User", userSchema)
