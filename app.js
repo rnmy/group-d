6d5e27788bee01a03b36e7d5ad863a51209d1d5e
tnsyn <e0425292@u.nus.edu>
2020-07-08 16:23:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const Event = require("./models/event"),
       User = require("./models/user"),
       Group = require("./models/group");
 
-// seedDB();
+//seedDB();
 
 // REQUIRING ROUTES
 const indexRoutes = require("./routes/index"),
