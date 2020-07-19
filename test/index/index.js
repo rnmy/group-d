35fb4873323bad30a25de9a656e34a981081604f
rnmy <e0439305@u.nus.edu>
2020-07-19 22:51:47 +0800

    Update index.js

--- test/index/index.js
+++ test/index/index.js
@@ -3,6 +3,7 @@ const chaiHttp = require('chai-http')
 const request = require('supertest')
 const app = require('../../app.js')
 const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
 const db = require('../db-handler')
 const expect = require('chai').expect 
 const assert = require('chai').assert 
@@ -79,6 +80,68 @@ describe("POST /register", () => {
         .end((err, res) => {
             expect(res).to.redirect
             expect(res.req.path).to.equal('/register')
+            done()
+        })
+    })
+})
+
+describe("GET /login", () => {
+    it("Login page loads", (done) => {
+        chai.request(app)
+        .get("/login")
+        .end((err, res) => {
+            expect(res.status).to.equal(200)
+            expect(res.text).to.have.string('<h1>Login!</h1>')
+            expect(res.req.path).to.equal('/login')
+            done()
+        })
+    })
+})
+
+const DefaultUserSchema = new mongoose.Schema()
+DefaultUserSchema.plugin(passportLocalMongoose)
+const DefaultUser = mongoose.model('DefaultUser', DefaultUserSchema)
+
+describe("POST /login", () => {
+    let TestUser
+    before(async () => {
+        TestUser = await new DefaultUser({ username: 'user' });
+        await TestUser.setPassword('password')
+        await TestUser.save()
+    })
+    after(async () => db.clearDatabase())
+    it('Should authenticate existing user with matching password', function(done) {
+        DefaultUser.authenticate()('user', 'password', function(err, result) {
+            if (err) {
+                return done(err);
+            }
+
+            expect(result instanceof DefaultUser).to.exist;
+            expect(result.username).to.equal(TestUser.username);
+
+            expect(result.salt).to.equal(TestUser.salt);
+            expect(result.hash).to.equal(TestUser.hash);
+        });
+
+        done()
+    });
+
+    it('should not authenticate existing user with non matching password', async () => {
+        const { user: result, error } = await DefaultUser.authenticate()('user', 'wrongpassword');
+
+        expect(result).to.equal(false);
+        expect(error).to.exist;
+    });
+})
+
+describe("GET /logout", () => {
+    it("Logout page loads", (done) => {
+        chai.request(app)
+        .get("/logout")
+        .end((err, res) => {
+            expect(res.status).to.equal(200)
+            expect(res.text).to.have.string("Welcome to <strong>group'd!</strong>\n")
+            expect(res.req.path).to.equal('/')
             done()
         })
     })
